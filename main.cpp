f1ab24bece64d34078f39cfa1d00fd2628806a0e
Gleb Kuzmin <glebushkauzmin2015@gmail.com>
2024-01-09 18:28:05 +0500

    modified:   .gitignore 	modified:   CmakeLists.txt 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -106,16 +106,17 @@ struct Vertex {
     }
 };
 
-const std::vector<Vertex> vertices = {
+std::vector<Vertex> vertices = {
     {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
     {{0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
     {{0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
     {{-0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}}
 };
 
-const std::vector<uint16_t> indices = {
+std::vector<uint16_t> indices = {
     0, 1, 2, 2, 3, 0
 };
+
 
 class HelloTriangleApplication {
 public:
